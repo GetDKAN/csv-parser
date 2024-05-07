1801d9eda3f7ee7d82a59b6cc74300b4019d41ba
Paul Mitchum <paul@mile23.com>
2024-05-07 10:54:15 -0700

    type hints

--- src/Parser/ParserInterface.php
+++ src/Parser/ParserInterface.php
@@ -2,13 +2,39 @@
 
 namespace CsvParser\Parser;
 
+/**
+ * Interface for parsing CSV files.
+ */
 interface ParserInterface
 {
-    public function feed(string $chunk);
 
+    /**
+     * Feed a string into the parser.
+     *
+     * @param string $chunk
+     *   A chunk of the CSV being fed into the parser.
+     */
+    public function feed(string $chunk): void;
+
+    /**
+     * Get a record.
+     *
+     * @return array|null
+     *   The record as an array, or null.
+     */
     public function getRecord();
 
-    public function reset();
+    /**
+     * Reset the parser to an initialized state.
+     */
+    public function reset(): void;
 
-    public function finish();
+    /**
+     * At the end of a run of parsing, ensure there were no errors.
+     *
+     * @throws \Exception
+     *   Throws an exception if the state machine was out of sync with our
+     *   expectations.
+     */
+    public function finish(): void;
 }
